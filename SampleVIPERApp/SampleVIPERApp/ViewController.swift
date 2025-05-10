98053959971335af220eaaaa18895b1a6064cd33
Rustam Shakirzianov <rustam4m@Mac-mini-Rustam.local>
2025-05-10 22:54:24 +0300

    Configured to Storyboard Free

---
+++ SampleVIPERApp/SampleVIPERApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleVIPERApp
+//
+//  Created by Rustam Shakirzianov on 10.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
